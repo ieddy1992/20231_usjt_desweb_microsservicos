e472cf8c57424d1bccb0891acff499beb4c7f7f3
ieddy1992 <ed_saopaulo@hotmail.com>
2023-03-31 21:20:06 -0300

    microservicos2023 Copia

--- lembretes/index.js
+++ lembretes/index.js
@@ -1,19 +1,33 @@
 const express = require('express');
+const axios = require('axios');
+
 const app = express()
 app.use(express.json())
 const lembretes = {};
 contador = 0;
+
 app.get('/lembretes', (req, res) => {
   res.send(lembretes);
 });
-app.post('/lembretes', (req, res) => {
+
+app.post('/lembretes', async(req, res) => {
   contador++;
   const { texto } = req.body;
   lembretes[contador] = {
     contador, texto
   }
+  await axios.post("http://localhost:10000/eventos",{
+    tipo: "Lembrete criado",
+    dados:{
+      contador, texto
+    }
+  });
   res.status(201).send(lembretes[contador]);
 });
+app.post("/eventos,(req, res)=>{
+  console.log(req.body);
+  res.status(200).send({msg: "ok"});
+})
 
 app.listen(4000, () => {
   console.log('Lembretes. Porta 4000');
--- observacoes/index.js
+++ observacoes/index.js
@@ -1,20 +1,27 @@
+const { default: axios } = require('axios');
 const express = require('express');
-const app = express();
+const app = express('axios');
 const { v4: uuidv4 } = require('uuid');
 app.use(express.json());
 const observacoesPorLembreteId = {};
 
 //:id é um placeholder
 //exemplo: /lembretes/123456/observacoes
-app.post('/lembretes/:id/observacoes', (req, res) => {
+app.post('/lembretes/:id/observacoes', async(req, res) => {
   const idObs = uuidv4();
   const { texto } = req.body;
   //req.params dá acesso à lista de parâmetros da URL
   const observacoesDoLembrete =
     observacoesPorLembreteId[req.params.id] || [];
   observacoesDoLembrete.push({ id: idObs, texto });
-  observacoesPorLembreteId[req.params.id] =
-    observacoesDoLembrete;
+  observacoesPorLembreteId[req.params.id] = observacoesDoLembrete;
+
+  await axios.post("http://localhost:5000/observacoes",{
+    tipo: "Observação Criada",
+    dados{
+      id: idObs, texto, lembreteId: req.params.id
+    }
+  })
   res.status(201).send(observacoesDoLembrete);
 });
 
